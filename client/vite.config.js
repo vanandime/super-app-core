9b059f64c56d7ada965be772ad4499107c96134a
vanandime <vanandi@proton.me>
2026-08-14 07:46:42 +0800

    Update comment for base URL in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/super-app-core/', // <-- Wajib diisi sesuai nama repository GitHub Pages Anda
+  base: '/super-app-core/', // Wajib sesuai dengan nama repository GitHub Anda
 });
